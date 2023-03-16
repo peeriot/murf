947543524b08d19d5813cc24e1368398b0bf038b
Bergmann89 <info@bergmann89.de>
2023-03-16 20:25:55 +0100

    Improve handling of the `Self` type

--- gmock/src/lib.rs
+++ gmock/src/lib.rs
@@ -2,7 +2,6 @@ pub mod action;
 pub mod matcher;
 pub mod sequence;
 pub mod times;
-pub mod traits;
 
 pub use gmock_macros::{expect_call, mock};
 
@@ -10,4 +9,3 @@ pub use action::Action;
 pub use matcher::Matcher;
 pub use sequence::{InSequence, Sequence, SequenceHandle};
 pub use times::{Times, TimesRange};
-pub use traits::{FromState, IntoState};
